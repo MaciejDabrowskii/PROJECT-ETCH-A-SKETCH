9076bd71ab173d53e2ae001544c7257baeb41a58
Jimmy <dabrowski.mac.1@gmail.com>
2022-01-09 11:45:25 +0100

    final version with style

--- skrypt.js
+++ skrypt.js
@@ -2,9 +2,46 @@ let numberOfSquares = 16;
 let divNumber = 0;
 let divMain = document.querySelector('.mainDiv');
 let pola = document.querySelectorAll('pola');
+let button = document.querySelector('button');
+let numberOfResets = 0;
 addDivs();
 
+button.addEventListener('click', function (e) {
+    removeMainDiv();
+    addMainDiv();
+    addDivs();
+})
+
+function removeMainDiv() {
+    let body = document.querySelector('.napisPole');
+    body.removeChild(divMain);
+} 
+
+function addMainDiv() {
+    let body = document.querySelector('.napisPole');
+    divMain = document.createElement('div');
+    divMain.classList.add('mainDiv');
+    body.appendChild(divMain);
+    
+}
+
+function countingPixels() {
+    let pixels = 960/numberOfSquares;
+    return pixels;
+}
+
 function addDivs() {
+    if(numberOfResets >= 1){
+        let enteredNumber = Number(prompt('Podaj ilość pól w lini MAX = 100'));
+            if(enteredNumber > 100 || enteredNumber < 0 || isNaN(enteredNumber)) {
+                alert('Wprowadzona wartość nie miesci się w zakresie 1-100 lub nie jest numerem');
+                return addDivs();
+            } else if (enteredNumber === 0) {
+                alert('Anulowano');
+                return 0;
+        };
+        numberOfSquares = enteredNumber;
+    }
     for (let i = 0; i < numberOfSquares; i++) {
         let divrow = document.createElement(`div`);
         divrow.classList.add(`divRow`);
@@ -16,15 +53,12 @@ function addDivs() {
             let div = document.createElement('div');
             div.classList.add('pola');
             div.addEventListener("mouseenter", function (e) {
-                div.classList.replace('pola', 'polaColor');
+            div.classList.replace('pola', 'polaColor');
             })
             divrow.appendChild(div);
         };
         
         }
+    numberOfResets += 1;
 }
 
-function countingPixels() {
-    let pixels = 960/numberOfSquares;
-    return pixels;
-}
